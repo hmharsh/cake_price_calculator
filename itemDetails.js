db2cb635e05a296eae31dc07d59aa2c46e8e1fe1
harshit mahajan <harshit.mahajan@kyndryl.com>
2022-10-06 22:58:45 +0530

    initial commit

---
+++ itemDetails.js
@@ -0,0 +1,54 @@
+var itemDetails = 
+    [
+        {
+            "name": "Maida",
+            "unit": "gram",
+            "price": ".049",
+            "brand": "gangwal",
+            "comment": ""
+        },
+        {
+            "name": "Rawa",
+            "unit": "gram",
+            "price": ".049",
+            "brand": "gangwal",
+            "comment": ""
+        },
+        {
+            "name": "Brown sugar",
+            "unit": "gram",
+            "price": ".1",
+            "brand": "mawana",
+            "comment": ""
+        },
+        {
+            "name": "wheat flour",
+            "unit": "gram",
+            "price": ".062",
+            "brand": "ashirwad",
+            "comment": ""
+        },
+        {
+            "name": "jaggery powder",
+            "unit": "gram",
+            "price": ".15",
+            "brand": "pro nature organic",
+            "comment": ""
+        },
+        {
+            "name": "flavourless oil",
+            "unit": "gram",
+            "price": ".155",
+            "brand": "fortune soya",
+            "comment": ""
+        },
+        {
+            "name": "Brown sugar",
+            "unit": "gram",
+            "price": ".1",
+            "brand": "mawana",
+            "comment": ""
+        }
+
+        
+    ]
