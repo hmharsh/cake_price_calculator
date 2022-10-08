bfd2e28cba3cfde0f56ffa51adc3f24ca5519cad
harshit mahajan <harshit.mahajan@kyndryl.com>
2022-10-08 12:05:41 +0530

    variabalize the price and add favicon

--- itemDetails.js
+++ itemDetails.js
@@ -4,42 +4,42 @@ var itemDetails =
             "name": "Maida",
             "unit": "gram",
             "price": ".049",
-            "brand": "gangwal",
+            "brand": "Gangwal",
             "comment": ""
         },
         {
             "name": "Rawa",
             "unit": "gram",
             "price": ".049",
-            "brand": "gangwal",
+            "brand": "Gangwal",
             "comment": ""
         },
         {
             "name": "wheat flour",
             "unit": "gram",
             "price": ".062",
-            "brand": "ashirwad",
+            "brand": "Ashirwad",
             "comment": ""
         },
         {
             "name": "jaggery powder",
             "unit": "gram",
             "price": ".15",
-            "brand": "pro nature organic",
+            "brand": "Pro nature organic",
             "comment": ""
         },
         {
             "name": "flavourless oil",
             "unit": "ml",
             "price": ".155",
-            "brand": "fortune soya",
+            "brand": "Fortune soya",
             "comment": ""
         },
         {
             "name": "Brown sugar",
             "unit": "gram",
             "price": ".1",
-            "brand": "mawana",
+            "brand": "Mawana",
             "comment": ""
         }
 
