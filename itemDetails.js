5fc726b07c8eabc8a4bf7097cb36ddc5a282aba0
harshit mahajan <harshit.mahajan@kyndryl.com>
2022-10-09 22:07:28 +0530

    add more items

--- itemDetails.js
+++ itemDetails.js
@@ -3,7 +3,7 @@ var itemDetails =
         {
             "name": "Maida",
             "unit": "gram",
-            "price": ".049",
+            "price": ".055",
             "brand": "Gangwal",
             "seller": "",
             "comment": ""
@@ -11,7 +11,7 @@ var itemDetails =
         {
             "name": "Rawa",
             "unit": "gram",
-            "price": ".049",
+            "price": ".08",
             "brand": "Gangwal",
             "seller": "",
             "comment": ""
@@ -47,7 +47,136 @@ var itemDetails =
             "brand": "Mawana",
             "seller": "",
             "comment": ""
-        },{
+        },
+        {
+            "name": "Baking powder",
+            "unit": "gram",
+            "price": ".225",
+            "brand": "winkfield",
+            "seller": "Bakewala",
+            "comment": ""
+        },
+        {
+            "name": "Baking soda",
+            "unit": "gram",
+            "price": ".3",
+            "brand": "winkfield",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Glutan free flour",
+            "unit": "gram",
+            "price": ".15",
+            "brand": "Jiwa",
+            "seller": "",
+            "comment": ""
+        },     
+        {
+            "name": "Castor sugar",
+            "unit": "gram",
+            "price": ".13",
+            "brand": "Mawana",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Cocoa powder",
+            "unit": "gram",
+            "price": ".1",
+            "brand": "winkfield",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Xanthan gum",
+            "unit": "gram",
+            "price": "1.225",
+            "brand": "Urban platter",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Chocolate soya milk",
+            "unit": "gram",
+            "price": ".175",
+            "brand": "Sofit",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Vinegar",
+            "unit": "gram",
+            "price": ".623",
+            "brand": "Heinz",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Multi grain atta",
+            "unit": "gram",
+            "price": ".085",
+            "brand": "24 mantra organic",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Ragi atta",
+            "unit": "gram",
+            "price": ".09",
+            "brand": "24 mantra organic",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Butter",
+            "unit": "gram",
+            "price": ".492",
+            "brand": "Amul",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Curd",
+            "unit": "gram",
+            "price": ".09",
+            "brand": "Milky mist",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Dark compound",
+            "unit": "gram",
+            "price": ".25",
+            "brand": "Vanleer, 2M",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Vanilla essence",
+            "unit": "ml",
+            "price": "1.5",
+            "brand": "symega",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Instant coffee powder",
+            "unit": "gram",
+            "price": "1.34",
+            "brand": "Nescafe",
+            "seller": "",
+            "comment": ""
+        },
+        {
+            "name": "Sour cream",
+            "unit": "gram",
+            "price": ".325",
+            "brand": "Amul",
+            "seller": "",
+            "comment": ""
+        },
+        {
             "name": "Helper charges",
             "unit": "hour",
             "price": "80",
