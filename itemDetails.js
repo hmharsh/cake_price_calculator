3e0422cf5bfd8a82af5ba5bb775166eba7b197ac
harshit mahajan <harshit.mahajan@kyndryl.com>
2022-11-06 16:36:52 +0530

    with more ingredients

--- itemDetails.js
+++ itemDetails.js
@@ -177,12 +177,283 @@ var itemDetails =
             "comment": ""
         },
         {
+            "name": "Milk",
+            "unit": "ml",
+            "price": ".046",
+            "brand": "Nandini",
+            "seller": "Dairy",
+            "comment": ""
+        },
+        {
+            "name": "Alomod powder",
+            "unit": "gram",
+            "price": ".2.77",
+            "brand": "Weissmill",
+            "seller": "Bakewala",
+            "comment": "100 gram pouch"
+        },
+        {
+            "name": "Condensed Milk",
+            "unit": "gram",
+            "price": ".29",
+            "brand": "Amul",
+            "seller": "Reliance",
+            "comment": "Amul methai mate 200 gram tin pack"
+        },
+        {
+            "name": "Cream cheese",
+            "unit": "gram",
+            "price": "1.27",
+            "brand": "D'Lecta",
+            "seller": "Bakers junction, zepto",
+            "comment": "150 gram pack with MRP of 190"
+        },
+        {
+            "name": "Apple cider vinegar",
+            "unit": "ml",
+            "price": "0.391",
+            "brand": "American garden",
+            "seller": "Big-Basket",
+            "comment": "473 ml bottel in 185 MRP on big basket"
+        },
+        {
+            "name": "Olive oil",
+            "unit": "ml",
+            "price": "1.5",
+            "brand": "Delmonte",
+            "seller": "Reliance/Nearby showroom/big basket",
+            "comment": "100 ml bottel in 150"
+        },
+        {
+            "name": "Oats",
+            "unit": "ml",
+            "price": ".27",
+            "brand": "Saffola",
+            "seller": "big basket",
+            "comment": "53 rupees pack for 200 gram"
+        },
+        {
+            "name": "Kolhapuri Jaggery",
+            "unit": "gram",
+            "price": ".13",
+            "brand": "BB royal",
+            "seller": "big basket",
+            "comment": "400 gram pack in 53"
+        },
+        {
+            "name": "Suji/Rawa",
+            "unit": "gram",
+            "price": ".04",
+            "brand": "Gangwal/BB royal",
+            "seller": "big basket",
+            "comment": "40 Rupees per kg - BB Royal"
+        },
+        {
+            "name": "Un-salted butter",
+            "unit": "gram",
+            "price": ".53",
+            "brand": "Amul",
+            "seller": "Reliance / big basket",
+            "comment": "500 gram in 265 rupees"
+        },
+        {
+            "name": "Light brown cane sugar",
+            "unit": "gram",
+            "price": ".55",
+            "brand": "Tate & lyle",
+            "seller": "Amazon",
+            "comment": "550 rupees per 1000 gram"
+        },
+        {
+            "name": "Dermera sugar",
+            "unit": "gram",
+            "price": ".15",
+            "brand": "blue bird / Weissmill",
+            "seller": "Amazon",
+            "comment": "150 rupees per kg"
+        },
+        {
+            "name": "Light brown sugar",
+            "unit": "gram",
+            "price": ".75",
+            "brand": "Tate & lyle",
+            "seller": "Amazon",
+            "comment": "750 rupees per kg"
+        },
+        {
+            "name": "Rajgira aata",
+            "unit": "gram",
+            "price": ".195",
+            "brand": "Jiwa",
+            "seller": "Amazon",
+            "comment": "195 per KG"
+        },
+        {
+            "name": "Dessicated cococnut",
+            "unit": "gram",
+            "price": ".37",
+            "brand": "BB royal",
+            "seller": "big basket",
+            "comment": "37 rupees pouch of 100 gram, BB royal"
+        },
+        {
+            "name": "Ghee",
+            "unit": "ml",
+            "price": ".57",
+            "brand": "Amul",
+            "seller": "blinkit",
+            "comment": "500 ml pack"
+        },
+        {
+            "name": "Amul fresh cream",
+            "unit": "ml",
+            "price": ".27",
+            "brand": "Amul",
+            "seller": "Reliance",
+            "comment": "250 ml for 67 rupees"
+        },
+        {
+            "name": "Mawa/khova",
+            "unit": "gram",
+            "price": ".39",
+            "brand": "Milky Mist",
+            "seller": "Reliance/big basket",
+            "comment": "200 gram for 78 rupees"
+        },
+        {
+            "name": "Dark choco chips",
+            "unit": "gram",
+            "price": ".65",
+            "brand": "Ask foods",
+            "seller": "Bakewala/amazon",
+            "comment": "100 gram pouch with 65 MRP"
+        },
+        {
+            "name": "Duel choco chips",
+            "unit": "gram",
+            "price": ".85",
+            "brand": "Ask foods",
+            "seller": "Bakewala/amazon",
+            "comment": "100 gram pouch with 85 MRP"
+        },
+        {
+            "name": "white choco chips",
+            "unit": "gram",
+            "price": ".80",
+            "brand": "Ask foods",
+            "seller": "Bakewala/amazon",
+            "comment": "100 gram pouch with 80 MRP"
+        },
+        {
+            "name": "Rose essence",
+            "unit": "ml",
+            "price": "3.8",
+            "brand": "Fab",
+            "seller": "Bakewala",
+            "comment": "30 ml bottel container with 115 MRP"
+        },
+        {
+            "name": "Oil base color",
+            "unit": "gram",
+            "price": "7.2",
+            "brand": "Magic colors",
+            "seller": "Bakewala",
+            "comment": "25 gram bottel 180 MRP"
+        },
+        {
+            "name": "Pista essence",
+            "unit": "gram",
+            "price": "13",
+            "brand": "Fab",
+            "seller": "Bakewala",
+            "comment": "30 gram bottel 390 MRP"
+        },
+        {
+            "name": "Almond essence",
+            "unit": "gram",
+            "price": "3.8",
+            "brand": "Fab",
+            "seller": "Bakewala",
+            "comment": "30 gram bottel 115 MRP"
+        },
+        {
+            "name": "Peanut butter",
+            "unit": "gram",
+            "price": "0.5",
+            "brand": "Kissan",
+            "seller": "Reliance",
+            "comment": "350 gram bottel 175 MRP"
+        },
+        {
+            "name": "Peanuts",
+            "unit": "gram",
+            "price": "0.18",
+            "brand": "BB royal",
+            "seller": "Big basket",
+            "comment": "500 gram bottel 94 MRP"
+        },
+        {
+            "name": "Milk compound",
+            "unit": "gram",
+            "price": "0.432",
+            "brand": "Vanleer",
+            "seller": "bakewala",
+            "comment": "500 gram pack 216 MRP"
+        },
+        {
+            "name": "White compound",
+            "unit": "gram",
+            "price": "0.56",
+            "brand": "Vanleer",
+            "seller": "bakewala",
+            "comment": "500 gram pack 280 MRP"
+        },
+        {
             "name": "Helper charges",
-            "unit": "hour",
-            "price": "80",
+            "unit": "half hour",
+            "price": "40",
+            "brand": "-",
+            "seller": "-",
+            "comment": "-"
+        },
+        {
+            "name": "Baker charges",
+            "unit": "half hour",
+            "price": "63",
+            "brand": "-",
+            "seller": "-",
+            "comment": "-"
+        },
+        {
+            "name": "shipping charges",
+            "unit": "product",
+            "price": "20",
+            "brand": "-",
+            "seller": "-",
+            "comment": "avarage calculation"
+        },
+        {
+            "name": "Elecricity and gas charges",
+            "unit": "half hour",
+            "price": "10",
+            "brand": "-",
+            "seller": "-",
+            "comment": "Avarage based on per unit charges in banglore and 1500 watt OTG"
+        },
+        {
+            "name": "Cookie pizza packaging",
+            "unit": "noc",
+            "price": "35",
+            "brand": "-",
+            "seller": "-",
+            "comment": "-"
+        },
+        {
+            "name": "Cake packaging",
+            "unit": "noc",
+            "price": "40",
             "brand": "-",
             "seller": "-",
             "comment": "-"
         }
-
     ]
